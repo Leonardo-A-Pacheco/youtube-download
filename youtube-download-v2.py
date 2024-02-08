d5eb27b0ebc488d295b75693b2a045202162ba99
Leonardo-A-Pacheco <l00000111.pacheco@gmail.com>
2024-02-08 19:14:47 -0300

    OO-project.py

---
+++ youtube-download-v2.py
@@ -0,0 +1,36 @@
+from pytube import YouTube
+
+class Downloader:
+    def __init__(self, links):
+        self.links = links
+
+    def baixar_videos(self):
+        for link in self.links:
+            try:
+                yt = YouTube(link)
+                self.baixar_video(yt)
+            except Exception as e:
+                print(f'Ocorreu um erro ao baixar o vídeo {link}: {e}')
+
+    def baixar_video(self, yt):
+        try:
+            video = yt.streams.get_highest_resolution()
+            print(f'Baixando vídeo: {yt.title}')
+            video.download(output_path='downloads/')
+            print('Download concluído!')
+        except Exception as e:
+            print(f'Ocorreu um erro ao baixar o vídeo {yt.title}: {e}')
+
+
+if __name__ == "__main__":
+    # Lista de links dos vídeos do YouTube
+    links = [
+        # 'https://www.youtube.com/watch?v=VIDEO_ID_1',
+        # 'https://www.youtube.com/watch?v=VIDEO_ID_2',
+        # Adicione mais links conforme necessário
+        'https://www.youtube.com/watch?v=LtsAiAbeFvI'
+
+    ]
+
+    downloader = Downloader(links)
+    downloader.baixar_videos()
